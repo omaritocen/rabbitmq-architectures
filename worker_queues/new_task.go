801daa1441818d53cdc859e0c637b039897da72d
omaritocen <omaralaaabdelkader@gmail.com>
2023-04-17 02:30:39 +0200

    Refactor folders to work_queues

--- worker_queues/new_task.go
+++ worker_queues/new_task.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"log"
 	"os"
+	"rabbitmqhello/utils"
 	"strings"
 	"time"
 
@@ -12,11 +13,11 @@ import (
 
 func main() {
 	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672")
-	failOnError(err, "Failed to connect to RabbitMQ")
+	utils.FailOnError(err, "Failed to connect to RabbitMQ")
 	defer conn.Close()
 
 	ch, err := conn.Channel()
-	failOnError(err, "Failed to Open a channel")
+	utils.FailOnError(err, "Failed to Open a channel")
 	defer ch.Close()
 
 	q, err := ch.QueueDeclare(
@@ -27,7 +28,7 @@ func main() {
 		false,
 		nil,
 	)
-	failOnError(err, "Failed to declare a queue")
+	utils.FailOnError(err, "Failed to declare a queue")
 
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
@@ -44,7 +45,7 @@ func main() {
 			ContentType:  "text/plain",
 			Body:         []byte(body),
 		})
-	failOnError(err, "Failed to publish a message")
+	utils.FailOnError(err, "Failed to publish a message")
 	log.Printf(" [x] Sent %s\n", body)
 }
 
@@ -57,10 +58,4 @@ func bodyFrom(args []string) string {
 	}
 
 	return s
-}
-
-func failOnError(err error, msg string) {
-	if err != nil {
-		log.Panicf("%s %s", msg, err)
-	}
 }
--- worker_queues/worker.go
+++ worker_queues/worker.go
@@ -4,16 +4,17 @@ import (
 	"bytes"
 	amqp "github.com/rabbitmq/amqp091-go"
 	"log"
+	"rabbitmqhello/utils"
 	"time"
 )
 
 func main() {
 	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
-	failOnError(err, "Failed to connect to RabbitMQ")
+	utils.FailOnError(err, "Failed to connect to RabbitMQ")
 	defer conn.Close()
 
 	ch, err := conn.Channel()
-	failOnError(err, "Failed to open a channel")
+	utils.FailOnError(err, "Failed to open a channel")
 	defer ch.Close()
 
 	q, err := ch.QueueDeclare(
@@ -24,14 +25,14 @@ func main() {
 		false,        // no-wait
 		nil,          // arguments
 	)
-	failOnError(err, "Failed to declare a queue")
+	utils.FailOnError(err, "Failed to declare a queue")
 
 	err = ch.Qos(
 		1,
 		0,
 		false,
 	)
-	failOnError(err, "Failed to set QoS")
+	utils.FailOnError(err, "Failed to set QoS")
 
 	msgs, err := ch.Consume(
 		q.Name,
@@ -58,10 +59,4 @@ func main() {
 
 	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
 	<-forever
-}
-
-func failOnError(err error, msg string) {
-	if err != nil {
-		log.Panicf("%s %s", msg, err)
-	}
 }
